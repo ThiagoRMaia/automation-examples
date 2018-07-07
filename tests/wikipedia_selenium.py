19c1f6d1d0ee1e7f6c976aab40715f1a7da1028b
Thiago Rocha Maia <rmaia.thiago@gmail.com>
2018-07-07 13:13:50 -0300

    Changes on constants and wikipedia tests to work with new functions lib

--- tests/wikipedia_selenium.py
+++ tests/wikipedia_selenium.py
@@ -1,31 +1,13 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from util import constants
+from util import constants, common_func
 import unittest
-
-
-class Functions:
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def start_selenium():
-        chrome_driver = constants.PATHS['chromedriver']
-        driver = webdriver.Chrome(chrome_driver)
-        return driver
-
-    @staticmethod
-    def search_term(driver, term):
-        search_field = driver.find_element_by_id('searchInput')
-        search_field.send_keys(term)
-        search_field.send_keys(Keys.ENTER)
 
 
 class Tests(unittest.TestCase):
     def setUp(self):
-        self.driver = Functions.start_selenium()
-        self.driver.get('http://www.wikipedia.org')
-        self.elements = constants.ELEMENTS
+        self.elements = constants.WIKIPEDIA_ELEMENTS
+        self.functions = common_func.Functions()
+        self.driver = self.functions.start_selenium()
+        self.driver.get(self.elements['website'])
 
     def tearDown(self):
         self.driver.close()
@@ -34,18 +16,18 @@ class Tests(unittest.TestCase):
         self.assertEqual(self.driver.title, 'wikipedia', 'Title found: ' + self.driver.title + '\nExpected: wikipedia')
 
     def test_02_search(self):
-        Functions.search_term(self.driver, 'furry rabbits')
+        self.functions.search_term_wikipedia(self.driver, 'furry rabbits')
         self.assertTrue(self.driver.find_element_by_class_name(self.elements['do_you_mean']))
 
     def test_03_click_suggestion(self):
-        Functions.search_term(self.driver, 'furry rabbits')
+        self.functions.search_term_wikipedia(self.driver, 'furry rabbits')
         self.driver.find_element_by_id(self.elements['suggestion']).click()
         search_results = self.driver.find_element_by_class_name(self.elements['search_result'])
         children = search_results.find_elements_by_tag_name(self.elements['list_item'])
         self.assertEqual(len(children), 20, 'Elements found: ' + str(len(children)))
 
     def test_04_check_result_content(self):
-        Functions.search_term(self.driver, 'furry rabbits')
+        self.functions.search_term_wikipedia(self.driver, 'furry rabbits')
         self.driver.find_element_by_id(self.elements['suggestion']).click()
         self.driver.find_element_by_xpath(self.elements['first_result']).click()
         self.assertTrue(self.driver.find_element_by_id(self.elements['title']), 'Title not found')
